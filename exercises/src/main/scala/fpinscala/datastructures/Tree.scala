5adf0128bff36ba07c2558b30c02b7266dcbf540
Dmitriy Onishchenko <dmitriy.onishchenko@veon.com>
2017-06-04 22:22:54 +0200

    Trees: depth, map

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -15,4 +15,14 @@ object Tree {
     case Leaf(v) => v
     case Branch(l, r) => maximum(l) max maximum(r)
   }
+
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 }
