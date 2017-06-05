3b792a456f5929d8e8b2d16491aa3a66a472293f
Dmitriy <dimuon@gmail.com>
2017-06-05 11:33:30 +0300

    Trees: all

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -30,4 +30,17 @@ object Tree {
     case Leaf(v) => f(v)
     case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
   }
+
+  def sizeViaFold[A](t: Tree[A]): Int =
+    fold(t)(_ => 1)(1 + _ + _)
+
+  def maximumViaFold(t: Tree[Int]): Int =
+    fold(t)(a => a)(_ max _)
+
+  def depthViaFold[A](t: Tree[A]): Int =
+    fold(t)(_ => 0)((l: Int, r: Int) => 1 + (l max r))
+  
+  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
+
 }
